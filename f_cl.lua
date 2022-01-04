e2c05772314367f838073d3a35ee3d9889783d5e
karpo0 <40266114+karpo0@users.noreply.github.com>
2022-01-04 20:45:25 +0200

    Update f_cl.lua

--- f_cl.lua
+++ f_cl.lua
@@ -4,13 +4,8 @@ end)
 
 RegisterNetEvent('fcl:coordis')
 AddEventHandler('fcl:coordis', function()
-    Citizen.CreateThread(function()                             
-        while true do
-                local nimieikai = GetPlayerName(PlayerId())
-                local koordittossaja = GetEntityCoords(GetPlayerPed(-1))   
-                local suunta = GetEntityHeading(GetPlayerPed(-1)) 
-                TriggerServerEvent('fsv:coordis:laitappa', koordittossaja, suunta, nimieikai)
-                break
-           end
-       end)
-   end)                 
+    local nimieikai = GetPlayerName(PlayerId())
+    local koordittossaja = GetEntityCoords(GetPlayerPed(-1))   
+    local suunta = GetEntityHeading(GetPlayerPed(-1)) 
+    TriggerServerEvent('fsv:coordis:laitappa', koordittossaja, suunta, nimieikai)
+end)                 
